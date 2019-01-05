ab84e7969a3353b16212c62ffa13a63787e50e33
tavishjain <jaintavish@gmail.com>
2019-01-05 21:54:17 +0530

    Wwebsite redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "tavishjain");
+    GitHubCalendar("github-graph", "tavishjain");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
