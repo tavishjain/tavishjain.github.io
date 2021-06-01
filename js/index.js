740e8e7dbd0e0ac2837bc99b8f2abfcc00ef81a0
tavishjain <jaintavish@gmail.com>
2021-06-01 23:59:26 +0530

    Trying new site

--- js/index.js
+++ js/index.js
@@ -149,7 +149,7 @@ $(window).resize(onWindowResize);
 var profile;
 // swal({
 // 		title: "Hello World!!!",
-// 		text: "Hello visitor, you have landed upon little webspace of moghya. I hope you're doing well."
+// 		text: "Hello visitor, you have landed upon little webspace of tavish. I hope you're doing well."
 // 		// buttons: ["Nope, I'm just looking around.", "Yes, I'm hiring :)"]
 // 		// buttons: {
 // 		// 	cancel: {
@@ -199,15 +199,7 @@ var profile;
 
 
 function loadMoghysSays() {
-	const moghyaSaysInnerHtml = `<div class="col m6">
-		<h6>Recipe for this website:</h6>	
-		<div class="row">
-			Would you like to have your own portfolio in this template ? It"s pretty easy, <a href="https://github.com/moghya">moghya</a> covered it up for everyone out their. 
-			All the content on this website is dynamically loaded from JSON data.
-			Fork this <a href="https://github.com/moghya/moghya.github.io/">repo</a> on github and edit <a href="https://github.com/moghya/moghya.github.io/blob/master/js/profile.json">js/profile.json</a> for adding your data.
-			<a href="https://medium.com/howcatcancode/developer-profile-template-2017-219f43147efe">Read more</a><br>
-			If you like this website, consider giving a star to its repo <a href="https://github.com/moghya/moghya.github.io/">here</a>.
-		</div>
+	const tavishSaysInnerHtml = `<div class="col m6">
 	</div>
 	<div class="col m6">
 		<h6>Warm Gratitudes</h6>
@@ -230,7 +222,7 @@ function loadMoghysSays() {
 			<div class="col m3 s3"><a href="http://t4t5.github.io/sweetalert/">Sweetalert</a></div>
 		</div>
 	</div>`;
-	$('#moghyaSays').html(moghyaSaysInnerHtml);
+	$('#tavishSays').html(tavishSaysInnerHtml);
 }
 
 $.get("js/profile.json", 
@@ -253,13 +245,13 @@ $.get("js/profile.json",
 			<li class="tab col s3"><a href="#experience">Experience</a></li>
 			<li class="tab col s3"><a href="#education">Education</a></li>
 		`);
-		$('#believe').html('<h4>I believe</h4><span></span>');
-		const typed = new Typed('#believe span', {
-			strings: profile.qoutes,
-			typeSpeed: 40,
-			cursorChar:"_",
-			loop:true
-		});
+		// $('#believe').html('<h4>I believe</h4><span></span>');
+		// const typed = new Typed('#believe span', {
+		// 	strings: profile.qoutes,
+		// 	typeSpeed: 40,
+		// 	cursorChar:"_",
+		// 	loop:true
+		// });
 		loadLikes(profile.likes);
 		$('#helloText').html(profile.helloText);
 		loadLinks(profile.profileLinks);
